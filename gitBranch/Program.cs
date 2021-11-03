755cb399adb03ef45a468ba745429daa23b0b2a2
Erin Walter <erinwalter@rocketmortgage.com>
2021-11-03 15:08:25 -0400

    Erin's Edits

--- gitBranch/Program.cs
+++ gitBranch/Program.cs
@@ -7,6 +7,9 @@ namespace gitBranch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Erin is making edits to the program");
+            Console.WriteLine("This program is cool");
+            Console.WriteLine("Here are my edits to this program!");
         }
     }
 }
